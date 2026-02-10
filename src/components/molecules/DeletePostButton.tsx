b63ebb3f8b30b832b8fee57e5acf82c7a7cc3a99
Alejandro Curiel <jcuriel@tiprotec.com>
2026-02-10 16:52:03 -0600

    Agregar tipos de TS a DeletePostButton

--- src/components/molecules/DeletePostButton.tsx
+++ src/components/molecules/DeletePostButton.tsx
@@ -1,6 +1,11 @@
-import { usePostMutations } from "@/src/features/posts/hooks/usePostMutations";
+"use client";
 
-export function DeletePostButton({ post }) {
+import { usePostMutations } from "@/src/features/posts/hooks/usePostMutations";
+import { Post } from "@/src/features/posts/types/post";
+
+type Props = { post: Post };
+
+export function DeletePostButton({ post }: Props) {
   const { handleDelete } = usePostMutations();
 
   if (post.origin !== "local") return null;
