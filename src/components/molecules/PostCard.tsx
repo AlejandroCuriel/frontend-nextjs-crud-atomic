d8caaa962184f4ee9f0a21fbeecaced46971953e
Alejandro Curiel <jcuriel@tiprotec.com>
2026-02-10 16:58:13 -0600

    Actualizar estilo del cuerpo del PostCard

--- src/components/molecules/PostCard.tsx
+++ src/components/molecules/PostCard.tsx
@@ -8,7 +8,7 @@ export function PostCard({ post }: { post: Post }) {
         <h3 className="font-bold capitalize">{post.title}</h3>
         <PostActionsMenu post={post} />
       </div>
-      <p className="text-sm text-gray-600 capitalize">{post.body}</p>
+      <p className="text-sm text-gray-600 capitalize text-justify">{post.body}</p>
     </div>
   );
 }
