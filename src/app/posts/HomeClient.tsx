6b4fe729cf63850a1bd07863723338715c3c02bf
Alejandro Curiel <jcuriel@tiprotec.com>
2026-02-10 16:46:32 -0600

    Incluir tipos de TS para initialPosts

--- src/app/posts/HomeClient.tsx
+++ src/app/posts/HomeClient.tsx
@@ -2,18 +2,21 @@
 
 import { PostList } from "@/src/components/organisms/PostList";
 import { usePostStore } from "@/src/features/posts/store/postStore";
+import { Post } from "@/src/features/posts/types/post";
 import { useEffect } from "react";
 
-export default function HomeClient({ initialPosts }) {
+type Props = { initialPosts: Post[] };
+
+export default function HomeClient({ initialPosts }: Props) {
   const posts = usePostStore(s => s.posts);
   const setPosts = usePostStore(s => s.setPosts);
 
   useEffect(() => {
     if (posts.length === 0) {
       setPosts(
-        initialPosts.map(post => ({
+        initialPosts.map((post) => ({
           ...post,
-          origin: "api"
+          origin: "api" as const
         }))
       );
     }
@@ -24,5 +27,5 @@ export default function HomeClient({ initialPosts }) {
       ? posts.slice(0, 7)
       : initialPosts;
 
-  return <PostList posts={visiblePosts} />;
+  return <PostList posts={visiblePosts} />
 }
