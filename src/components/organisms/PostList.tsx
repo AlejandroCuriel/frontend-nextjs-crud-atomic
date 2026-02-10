902ab10a61c7188dad7f13d2693b9d50ff86b357
Alejandro Curiel <jcuriel@tiprotec.com>
2026-02-09 18:21:48 -0600

    Agregar estilos y clases para las card de post

--- src/components/organisms/PostList.tsx
+++ src/components/organisms/PostList.tsx
@@ -3,7 +3,7 @@ import { PostCard } from "@/src/components/molecules/PostCard";
 
 export function PostList({ posts }: { posts: Post[] }) {
   return (
-    <div className="grid gap-4 md:grid-cols-2">
+    <div className="grid gap-4 px-4 md:px-0 md:grid-cols-3 auto-rows-fr">
       {posts.map(p => (
         <PostCard key={p.id} post={p} />
       ))}
