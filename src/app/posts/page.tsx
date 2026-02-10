685daffa748981d46e7291a947389f4bcef01966
Alejandro Curiel <jcuriel@tiprotec.com>
2026-02-09 18:40:12 -0600

    Agregar encabezado y contenedor estilizado

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,13 +3,25 @@ import { PostList } from "@/src/components/organisms/PostList";
 import Link from "next/link";
 
 export default async function PostsPage() {
-  const posts = await PostService.getAll(5);
+  const posts = await PostService.getAll();
 
   return (
-    <main className="container mx-auto p-6">
-      <Link href={'/'}>Regresar</Link>
-      <h1 className="text-2xl font-bold mb-6">Posts</h1>
-      <PostList posts={posts} />
+    <main className="space-y-6">
+      <section id="header--imagen" className="relative">
+      </section>
+      <div className="container mx-auto">
+
+        <Link href={'/'}>Regresar</Link>
+
+        <section className="space-y-6 container mx-auto">
+          <h1 className="text-center text-2xl">Explora todos nuestros posts</h1>
+          <div className="w-5/6 mx-auto bg-[#f8f8f8] p-8 rounded-md shadow-md">
+
+            <PostList posts={posts} />
+          </div>
+        </section>
+      </div>
+
     </main>
   );
 }
